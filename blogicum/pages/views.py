6c8ebeb57dfb65dbfe798ef28eaf711e288c81c0
Artemii <tssopranos@gmail.com>
2026-05-26 23:36:53 +0300

    Тесты пройдены

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,13 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def about(request):
-    """Отображает страницу о проекте"""
-    template_name = "pages/about.html"
-    return render(request, template_name)
+def about(request) -> HttpResponse:
+    return render(request, 'pages/about.html')
 
 
-def rules(request):
-    """Отображает страницу правил"""
-    template_name = "pages/rules.html"
-    return render(request, template_name)
+def rules(request) -> HttpResponse:
+    return render(request, 'pages/rules.html')
